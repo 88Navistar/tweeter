c4b050b21ce17dc4c9e4219b3c4986684ddafda1
Rob Wallator <rob@rawimpressions.ca>
2020-07-16 03:26:08 +0000

    slideDown error warnings added to non compliant tweets, linted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,66 +1,37 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
 
 $(document).ready(function() {
 
-//Helper functions ------>>>
-//Escape xss attack
-const escape =  function(str) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
+  //Helper functions ------>>>
+  //Escape xss attack
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-//renderTweets loops through tweets, calls createTweetElement for each tweet and prepends to tweet container
-const renderTweets = function(tweets) {
+  //renderTweets loops through tweets, calls createTweetElement for each tweet and prepends to tweet container
+  const renderTweets = function(tweets) {
 
-for (const tweet of tweets) {
-  const $tweet = createTweetElement(tweet);
-  $('#tweets-container').prepend($tweet);
-  }
-}
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet);
+    }
+  };
 
-//loadTweets call the renderTweets and loads to /tweets through the DOM
-const loadTweets = () => {
-  $.ajax({ url: "/tweets",  method: "GET" })
-    .then(function(tweets) {
-      $('#tweets-container').empty();
-      renderTweets(tweets);
-    });
-};
-loadTweets ();
+  //loadTweets call the renderTweets and loads to /tweets through the DOM
+  const loadTweets = () => {
+    $.ajax({ url: "/tweets",  method: "GET" })
+      .then(function(tweets) {
+        $('#tweets-container').empty();
+        renderTweets(tweets);
+      });
+  };
+  loadTweets();
 
-const createTweetElement = function(tweet) {
-  const { user, content, created_at } = tweet;
-  const xssSafe = escape(content.text);
-  let $tweet = `<article class="tweet">
+  const createTweetElement = function(tweet) {
+    const { user, content, created_at } = tweet;
+    const xssSafe = escape(content.text);
+    let $tweet = `<article class="tweet">
     <header>
       <div>
         <img src="${user.avatars}" alt="Users Avatar">
@@ -78,31 +49,29 @@ const createTweetElement = function(tweet) {
         <i class="fa fa-heart" aria-hidden="false"></i>
       </div>
     </footer>
-    </article>`
+    </article>`;
 
-return $tweet;
-}
+    return $tweet;
+  };
 
-
-//renderTweets(data);
-
-
-//Listeners
-$("#form-tweets").on( "submit", function(event) {
-  event.preventDefault();
-  const data = $(this).serialize();
-  //const input = data.replace(/%20/g, "").split("=")[1];
+  //Listeners
+  $("#form-tweets").on("submit", function(event) {
+    event.preventDefault();
+  
+    const data = $(this).serialize();
+    //const input = data.replace(/%20/g, "").split("=")[1];
 
     if ($("#tweet-text").val().length > 140 || !$("#tweet-text").val()) {
-      $("#form-tweets").append($(`<div id="errMsg"> <p> Please tweet between 1 and 140 characters </> </div>`));
+      $(".errMsg").slideDown(400);
+      $(".errMsg p").text('Please tweet between 1 & 140 characters');
     } else {
-  $.ajax("/tweets", {method: 'POST', data: data}).then(function () {
-    loadTweets();
-    $("#tweet-text").val("")
-    $("#tweet-text").focus();
-
+      $.ajax("/tweets", {method: 'POST', data: data}).then(function() {
+        $(".errMsg").slideUp(100);
+        loadTweets();
+        $("#tweet-text").val("");
+        $("#tweet-text").focus();
+        $('.counter').val(140);
       });
     }
   });
-  
 }); //Document.ready closing brace
